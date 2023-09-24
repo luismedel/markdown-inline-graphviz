55d4146f1cac82b9c496e0f1cbd5fc30889a74d4
Luis Medel <luis@luismedel.com>
2023-09-25 01:23:12 +0200

    Fix dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ import os
 import sys
 from setuptools import setup
 
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 
 setup(
     name="Markdown Inline Graphviz Extension",
     version=VERSION,
     py_modules=["mdx_inline_graphviz"],
-    install_requires=["Markdown >= 3, < 4"],
+    install_requires=[
+        "Markdown >= 3, < 4",
+        "graphviz >= 0.20.1, < 1"
+    ],
     author="Luis Medel",
     author_email="luis@luismedel.com",
     description="Render inline graphs with Markdown and Graphviz",
