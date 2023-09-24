ddf9441a603e23a9cbac3beff08edd3eb3be5774
Luis Medel <luis@luismedel.com>
2023-09-25 00:58:06 +0200

    Black formatting

--- setup.py
+++ setup.py
@@ -2,27 +2,24 @@ import os
 import sys
 from setuptools import setup
 
-VERSION = '1.0'
+VERSION = "1.0"
 
 setup(
     name="Markdown Inline Graphviz Extension",
     version=VERSION,
     py_modules=["mdx_inline_graphviz"],
-    install_requires=['Markdown >= 3, < 4'],
+    install_requires=["Markdown >= 3, < 4"],
     author="Luis Medel",
     author_email="luis@luismedel.com",
     description="Render inline graphs with Markdown and Graphviz",
     license="MIT",
     url="https://github.com/luismedel/markdown-inline-graphviz",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-
-        'Intended Audience :: Developers',
-        'Topic :: Documentation',
-        'Topic :: Text Processing',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Documentation",
+        "Topic :: Text Processing",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
 )
